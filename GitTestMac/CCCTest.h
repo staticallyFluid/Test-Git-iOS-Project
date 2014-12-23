561daf36492f66024b406504c32c6659c2bc7248
macadmin <macadmin@macadmins-Mac.local>
2014-12-23 09:32:12 -0800

    added a test class

---
+++ GitTestMac/CCCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCCTest.h
+//  GitTestMac
+//
+//  Created by macadmin on 12/23/14.
+//  Copyright (c) 2014 CCC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCCTest : NSObject
+
+@end
---
+++ GitTestMac/CCCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCCTest.m
+//  GitTestMac
+//
+//  Created by macadmin on 12/23/14.
+//  Copyright (c) 2014 CCC. All rights reserved.
+//
+
+#import "CCCTest.h"
+
+@implementation CCCTest
+
+@end
